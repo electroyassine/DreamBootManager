3412247bfc60a5f2239363730a72493a6e86b101
electroyassine <81477795+electroyassine@users.noreply.github.com>
2025-10-06 20:40:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,11 @@
 PLUGIN_DIR="/usr/lib/enigma2/python/Plugins/Extensions/DreamBootManager"
 REPO_URL="https://raw.githubusercontent.com/electroyassine/DreamBootManager/main"
 
-# Création du dossier
 mkdir -p "$PLUGIN_DIR"
 
-# Téléchargement silencieux des fichiers
 wget -q -O "$PLUGIN_DIR/plugin.py" "$REPO_URL/plugin.py"
-wget -q -O "$PLUGIN_DIR/__init__.py" "$REPO_URL/__init__.py" 
+wget -q -O "$PLUGIN_DIR/__init__.py" "$REPO_URL/__init__.py"
 wget -q -O "$PLUGIN_DIR/plugin.png" "$REPO_URL/plugin.png"
 
-# Redémarrage GUI
 sleep 2
-[ -f "/etc/init.d/enigma2" ] && /etc/init.d/enigma2 restart
+/etc/init.d/enigma2 restart
